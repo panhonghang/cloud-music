f87b935aabfbaf0ff033c30d9256a7b3a2bc1646
pangonghang <1966656576@qq.com>
2020-05-11 23:19:19 +0800

    singerlist

--- pages/singer/singer.js
+++ pages/singer/singer.js
@@ -2,7 +2,9 @@ import {axios} from "../../util/util";
 
 Page({
   data: {
-    list:[]
+    list:[],
+    letterlist:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    singerlist:[]
   },
   onLoad() {},
   onShow(){
@@ -11,7 +13,7 @@ Page({
       method:"GET"
     }).then(res=>{
       this.setData({
-        list: res.data.list.artists
+        list: res.data.list.artists.slice(0,10)
       })
     })
   }
